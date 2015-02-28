65b2297f67d230944cf74b6aa88664eab4f12ea5
CCC toshiba <carlos.cevallos.castro@gmail.com>
2015-02-28 18:58:45 -0500

    Tutorial Android | Cap. 15 | UriPermission y AlertDialog
    Publicado el 14 de jul. de 2014
    En este capítulo exploramos algunos de los cambios en el esquema de seguridad de Android 4.4 (KitKat), así como el uso de diálogos para confirmar operaciones con el usuario. Igualmente pulimos dos bugs que tenía nuestra App.

--- app/src/main/java/aynimake/com/miscontactos/ListaContactosFragment.java
+++ app/src/main/java/aynimake/com/miscontactos/ListaContactosFragment.java
@@ -1,7 +1,9 @@
 package aynimake.com.miscontactos;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Fragment;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
@@ -86,10 +88,27 @@ public class ListaContactosFragment extends Fragment {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
-            case R.id.action_eliminar_contacto: eliminarContacto(item);
+            case R.id.action_eliminar_contacto:
+                String mensaje = "¿Esta seguro de eliminar los contactos seleccionados?";
+                confirmarAccion(item, mensaje);
                 return true;
-            default: return super.onOptionsItemSelected(item);
+            default:
+                return super.onOptionsItemSelected(item);
         }
+    }
+
+    private void confirmarAccion(final MenuItem item, String mensaje) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setIcon(R.drawable.ic_action_warning).setTitle("Confirmar Operación");
+        builder.setMessage(mensaje);
+        builder.setPositiveButton("SI", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                eliminarContacto(item);
+            }
+        });
+        builder.setNegativeButton("NO", null);
+        builder.show();
     }
 
     // Elimina el Contacto
@@ -102,15 +121,15 @@ public class ListaContactosFragment extends Fragment {
             // Posicion del contacto en el adaptador
             int posicion = array.keyAt(i);
             if (array.valueAt(i)) seleccion.add(adapter.getItem(posicion));
-
-            Intent intent = new Intent("listacontactos");
-            intent.putExtra("operacion", ContactReceiver.CONTACTO_ELIMINADO);
-            intent.putExtra("datos", seleccion);
-
-            getActivity().sendBroadcast(intent);
-
-            contactsListView.clearChoices();
         }
+
+        Intent intent = new Intent("listacontactos");
+        intent.putExtra("operacion", ContactReceiver.CONTACTO_ELIMINADO);
+        intent.putExtra("datos", seleccion);
+
+        getActivity().sendBroadcast(intent);
+
+        contactsListView.clearChoices();
     }
 
 
